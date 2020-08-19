787530c9623da2dead0638538df21268a3e5179e
Pierre Carré <pierre.carre78@gmail.com>
2020-08-19 19:17:25 +0200

    Initial commit

---
+++ src/main/java/io/github/pierrecarre/demo/oauth2server/Oauth2ServerDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.pierrecarre.demo.oauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerDemoApplication.class, args);
+    }
+
+}
